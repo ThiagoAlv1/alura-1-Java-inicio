07b2f1324923bee8b2b27903b8ae8b4344fcc719
Thiago <thiago___melo@hotmail.com>
2023-11-23 16:09:02 -0300

    String e casting

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,21 @@ public class Main {
         System.out.println("Ano de lançamento: " + anoLancamento);
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+
+        String mensagem = """
+                  Olá, mundo!
+                  Este é um Text Block.
+                  Ele permite escrever textos com múltiplas linhas
+                  sem precisar usar caracteres de escape ou quebras de linha manualmente ou concatenações.
+                  """;
+        System.out.println(mensagem);
+
+        double numeroDouble = 10.5;
+        int numeroInteiro = (int) numeroDouble; // Casting explícito de double para int
+        System.out.println(numeroInteiro);
+
     }
 }
